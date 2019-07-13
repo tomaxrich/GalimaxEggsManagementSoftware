2c703f98bf529ae489fc6f58a174fb205bb17d26
Thomas M R <34826076+tomaxrich@users.noreply.github.com>
2019-07-13 01:55:16 -0600

    Add files via upload

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Notification from './Notifications'
+import ProjectList from '../projects/ProjectList'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+    render (){
+        //console.log(this.props);
+        const { projects } = this.props;
+
+        return (
+            <div className="dashboard container">
+                <div className="row">
+                    <div className="col s12 m6">
+                        <ProjectList projects={projects} />
+                    </div>
+                    <div className="col s12 m5">
+                        <Notification />
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+}
+export default connect(mapStateToProps)(Dashboard)
---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notifactions = () => {
+    return (
+        <div>
+            <p>Notifications</p>
+        </div>
+    )
+}
+
+export default Notifactions
